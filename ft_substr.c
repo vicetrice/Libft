606ec5270463a7062632fa836e8101bc8449e863
Vicente Samuel Alvarado Gonzalez <valvarad@c3r3s6.42malaga.com>
2023-10-25 20:18:24 +0200

    contar primero el numero se memoria para alojar

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: valvarad <valvarad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 16:32:52 by valvarad          #+#    #+#             */
-/*   Updated: 2023/10/04 17:52:19 by valvarad         ###   ########.fr       */
+/*   Updated: 2023/10/25 20:17:49 by valvarad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*substr;
 	unsigned int	cont;
-
+		
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	cont = 0;
-	if (substr != NULL && *s)
+	if (substr == NULL)
+		return NULL;
+	if (!*s)
 	{
-		if (ft_strlen(s) < start)
-		{
-			return (ft_strdup(""));
-		}
-		while (cont < len)
-		{
-			substr[cont] = s[start + cont];
-			cont++;
-		}
 		substr[cont] = '\0';
 		return (substr);
 	}
-	return (NULL);
+	if (ft_strlen(s) < start)
+	{
+		free(substr);
+		return (ft_strdup(""));
+	}
+	while (cont < len)
+	{
+		substr[cont] = s[start + cont];
+		cont++;
+	}
+	substr[cont] = '\0';
+	return (substr);	
 }
