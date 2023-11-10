5e554233c75372335558fb3feecaa352f8a1bf06
vicetrice <vicentesalvaradog@gmail.com>
2023-11-10 12:54:00 +0100

    bug fixed rec - 1 -> rec

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,14 +6,14 @@
 /*   By: valvarad <valvarad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:54:04 by valvarad          #+#    #+#             */
-/*   Updated: 2023/10/04 19:16:21 by valvarad         ###   ########.fr       */
+/*   Updated: 2023/11/10 12:51:41 by valvarad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-unsigned int	prove(char const *set, char const c)
+int	prove(char const *set, char const c)
 {
 	size_t	lon;
 
@@ -37,7 +37,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	rec = ft_strlen(s1);
 	while (s1[cont] && prove(set, s1[cont]))
 		cont++;
-	while ((rec - 1) && prove(set, s1[rec - 1]))
+	while (rec && prove(set, s1[rec - 1]))
 		rec--;
 	if (cont > rec)
 		rec = cont;
